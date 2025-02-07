2c311162d472802cd850c604b303ab5d5cfe1ffb
Louis <yienlouis470@gmail.com>
2025-02-07 11:28:39 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "mysecretkey")
+    JSON_COMPACT = False
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+db.init_app(app)
+migrate = Migrate(app, db)
+
+api = Api(app)
+CORS(app)
