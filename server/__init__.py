e7395a75c089206dbb82ee35b335517f2d363e11
Louis <yienlouis470@gmail.com>
2025-02-07 10:31:58 +0300

    Initial commit

---
+++ server/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_restful import Api
+from models import db, bcrypt 
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("config.Config")  
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    Migrate(app, db)
+    CORS(app, supports_credentials=True)
+    api = Api(app)
+
+    from .routes import register_routes
+    register_routes(api)
+
+    return app
+
+app = create_app()
